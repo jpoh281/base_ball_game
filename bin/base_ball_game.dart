49426fcab6e49bc1372813ede0619914a6d5b3d9
jpoh281 <hjp4869@naver.com>
2022-06-26 23:37:43 +0900

    ♻️ 하위 클래스가 변함에 따라 조금씩 수정

--- bin/base_ball_game.dart
+++ bin/base_ball_game.dart
@@ -1,6 +1,9 @@
 import 'package:base_ball_game/controllers/base_ball_game_controller.dart';
+import 'package:base_ball_game/models/base_ball.dart';
+import 'package:base_ball_game/views/game_board.dart';
 
 void main(List<String> arguments) async {
-  BaseBallGameController controller = BaseBallGameController();
+  BaseBallGameController controller =
+      BaseBallGameController(GameBoard(), BaseBall());
   controller.initialize();
 }
