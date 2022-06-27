46ba63a37c66a52c910d6825c3b3fe2449715928
jpoh281 <hjp4869@naver.com>
2022-06-28 00:40:49 +0900

    ♻️ 자연스럽게 동작하기 위해 정규표현식 수정

--- lib/models/rule.dart
+++ lib/models/rule.dart
@@ -4,6 +4,7 @@ class Rule {
   static late final int maxInning;
 
   static RegExp batRegExp = RegExp('[1-9]{1}');
+  static RegExp numberOfBatRegExp = RegExp('^[1-9]{1}\$');
   static RegExp inningsRegExp = RegExp('[1-9]{1}\\d*');
 
   Rule._();
@@ -11,16 +12,16 @@ class Rule {
   static int parseMaxInning(String? answer) {
     var regExpResult = inningsRegExp.allMatches(answer ?? '').toList();
     if (regExpResult.isEmpty) {
-      throw FormatException('0 초과하는 숫자를 입력해주세요.');
+      throw FormatException('0 초과하는 숫자를 입력해주세요.\n');
     }
     int.parse(regExpResult[0].group(0)!);
     return int.parse(regExpResult[0].group(0)!);
   }
 
   static int parseNumberOfBat(String? answer) {
-    var regExpResult = batRegExp.allMatches(answer ?? '').toList();
+    var regExpResult = numberOfBatRegExp.allMatches(answer ?? '').toList();
     if (regExpResult.isEmpty) {
-      throw FormatException('[1-9] 사이의 숫자를 입력해주세요.');
+      throw FormatException('[1-9] 사이의 숫자만 입력해주세요.\n');
     }
     int.parse(regExpResult[0].group(0)!);
     return int.parse(regExpResult[0].group(0)!);
