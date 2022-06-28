1f9a1796c91a829b9a138eba28d7a9d85a98c8e7
jpoh281 <hjp4869@naver.com>
2022-06-29 01:13:51 +0900

    ♻️ NumberOfBat 추가

--- lib/models/mound.dart
+++ lib/models/mound.dart
@@ -1,30 +1,29 @@
-import 'package:base_ball_game/models/ball_mix.dart';
 import 'package:base_ball_game/models/batter.dart';
 import 'package:base_ball_game/models/pitcher.dart';
 import 'package:base_ball_game/models/ball_count.dart';
 
 /// 타석
 class Mound {
-  Mound(this.batter, this.pitcher);
-
   final Batter batter;
   final Pitcher pitcher;
+  final int numberOfBat;
+  Mound(this.batter, this.pitcher, this.numberOfBat);
 
   BallCount compete() {
     final BallCount ballCount = BallCount();
 
-    for(int i = 0; i < BallMix.numberOfBat; i++){
-      judgement(i, ballCount);
+    for (int i = 0; i < numberOfBat; i++) {
+      judge(i, ballCount);
     }
 
     return ballCount;
   }
 
-  void judgement(int index, BallCount ballCount) {
+  void judge(int index, BallCount ballCount) {
     var pitchingBall = pitcher.pitch(index);
     if (batter.didMissSwing(index, pitchingBall)) {
       ballCount.addStrike();
-      return ;
+      return;
     }
 
     if (batter.didLook(pitchingBall)) {
